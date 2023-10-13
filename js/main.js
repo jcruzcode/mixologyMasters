a88eabc4a1cc0198a505425ba859b0e18d942ae9
jcruzcode <jcruz.code@gmail.com>
2023-10-12 23:07:15 -0800

    Added an event listener for devices with touchscreens

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 document.querySelector('#cocktail').addEventListener('click', getDrink);
+// Use touchend for touch screen devices
 document.querySelector('#cocktail').addEventListener('touchend', getDrink);
 
 
