dcec2f3a56ed52ded7c991267051f050cefaf2e5
jcruzcode <jcruz.code@gmail.com>
2023-10-12 23:04:57 -0800

    Updated main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 document.querySelector('#cocktail').addEventListener('click', getDrink);
+document.querySelector('#cocktail').addEventListener('touchend', getDrink);
+
 
 let drinkIndex = 0;
 
